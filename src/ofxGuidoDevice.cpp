424246ff3c3f308d4d5fc89e7cbf9bb625881a12
Luis Garrido <luis@vagar.org>
2016-03-13 11:42:43 +0100

    First commit of k4rm (Thomas Coffy) ofxGuido addon in GitHub.

---
+++ src/ofxGuidoDevice.cpp
@@ -0,0 +1,18 @@
+/*
+ * ofxGuidoDevice.cpp
+ *
+ *  Created on: Mar 11, 2016
+ *      Author: luis
+ */
+
+#include <ofxGuidoDevice.h>
+
+ofxGuidoDevice::ofxGuidoDevice(){
+	// TODO Auto-generated constructor stub
+
+}
+
+ofxGuidoDevice::~ofxGuidoDevice(){
+	// TODO Auto-generated destructor stub
+}
+
